2cf00b19eb18af1e46a30d59218cb377c613b8b8
tsmancini <85450333+tsmancini@users.noreply.github.com>
2021-06-13 23:47:01 +0000

    Added a question to ask for the candidates name and print a welome message

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,8 +15,11 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  let candidateName = input.question("What is your name? ");
 
 }
+
+console.log("Hello," candidateName, "Welcome to the candidate surveyor")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
