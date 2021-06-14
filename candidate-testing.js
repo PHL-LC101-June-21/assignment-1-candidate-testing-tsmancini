f30eaecf6375df104459a095c614659c56066e69
tsmancini <85450333+tsmancini@users.noreply.github.com>
2021-06-14 23:57:17 +0000

    Changed the function to not initialize twice. Changed comparisons to have {}.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -11,31 +11,31 @@ let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
-question = "Who was the first American woman in space? "
-correctAnswer = "Sally Ride"
-candidateAnswer = ""
+question = "Who was the first American woman in space? ";
+correctAnswer = "Sally Ride";
+candidateAnswer = "";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  let candidateName = input.question("What is your name? ")
-  console.log("Hello,", candidateName, "and welcome to the candidate surveyor!")
-
+  candidateName = input.question("What is your name? ");
+  console.log("Hello,", candidateName, "and welcome to the candidate surveyor!");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-candidateAnswer = input.question(question)
-
+candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-if (candidateAnswer === correctAnswer)
+if (candidateAnswer === correctAnswer) {
   console.log("Correct!")
-  else console.log("Incorrect!")
+ }  else {
+      console.log("Incorrect!")
+ }
   
   let grade;
   
