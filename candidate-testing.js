7879db79782c55e0b8577c057ed1beebaa1df401
tsmancini <85450333+tsmancini@users.noreply.github.com>
2021-06-22 01:37:00 +0000

    Added part 2. Wasnt able to test the part that tells the user what the answer is because for some reason I get an error saying that the final module.exports area has an unexpected end of input...

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,6 +15,10 @@ question = "Who was the first American woman in space? ";
 correctAnswer = "Sally Ride";
 candidateAnswer = "";
 
+questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?",	"(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"]
+correctAnswers = ["Sally Ride", "true", "40", "Trajetory", "3"]
+
+
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("What is your name? ");
@@ -24,17 +28,28 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-candidateAnswer = input.question(question);
+candidateAnswers = [];
+
+for (let i = 0; i < questions.length; i = i + 1) {
+  candidateAnswers[i] = input.question(`${questions[i]}`)
 }
+console.log(candidateAnswers)
+// 0kay so this part works now!
+
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+/* This was the original 1.2c from part 1
 
 if (candidateAnswer === correctAnswer) {
-  console.log("Correct!")
+  console.log("Correct!");
  }  else {
-      console.log("Incorrect!")
+      console.log("Incorrect!");
  }
   
   let grade;
@@ -43,6 +58,8 @@ if (candidateAnswer === correctAnswer) {
   return grade;
 }
 
+*/
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
@@ -50,6 +67,15 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+// Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.
+
+for (let i = 0; i < candidateAnswers.length; i = i + 1) {
+  console.log(`You answered, ${candidateAnswers[i]}. The Correct answer is ${correctAnswers}`)
+  }
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
